848e20b8a543ce0d10bc5a50b414b53075b34bab
daniilakulov <sladkoeshka33@yandex.ru>
2022-08-21 12:04:26 +0700

    С трудом, но выполнил

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,55 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
 
+    public static void main(String[] args) {
+    task1();task2();task3();task4();
+    }
+
+    public static void task1() {
+        int[] salaries = generateRandomArray();
+        int sum = 0;
+        for (int i = 0; i < salaries.length; i++) {
+            sum += salaries[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+    }
+
+    public static void task2(){
+        int[] salaries = generateRandomArray();
+        int max = salaries[0];
+        int min = salaries[0];
+        for (int i = 0; i < salaries.length; i++) {
+            if (max < salaries.length) {
+                max = salaries[i];
+            }
+            if (min > salaries.length) {
+                min = salaries[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + min + " рублей. Максимальная сумма трат за день составила " + max + " рублей");
+    }
+
+    public static void task3() {
+        int [] salaries = generateRandomArray();
+        double sum = 0;
+        for( double salary : salaries){
+            sum += salary;
+        }
+        System.out.println("Средняя сумма трат за месяц составила " + sum / salaries.length + " рублей");
+
+    }
+
+    public static void task4(){
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for ( int i = reverseFullName.length - 1; i >= 0; i--){
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
